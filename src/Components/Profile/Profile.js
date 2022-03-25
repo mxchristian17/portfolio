0efa5e6f81a4e8d8c181b834b2b07034a09424b8
mxchristian17 <90223948+mxchristian17@users.noreply.github.com>
2022-03-25 16:11:16 -0300

    profile img size fixed

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -10,8 +10,8 @@ const Profile = (props) => {
     <div className="col-md-6 col-lg-4 text-center px-2 py-3 d-flex align-items-center">
       <div className="row d-flex align-items-center w-100">
 
-        <div className="col d-md-none text-start text-sm-center"><ProfileData /></div>
-          <div className="col">
+        <div className="col-8 d-md-none text-start text-sm-center"><ProfileData /></div>
+        <div className="col-4 col-md-12 p-0 p-sm-4">
 
           {loading && <div className="d-flex justify-content-center text-success">
             <div className="spinner-border m-auto" role="status">
@@ -19,7 +19,7 @@ const Profile = (props) => {
             </div>
           </div>} 
 
-          <img onLoad={() => setLoading(false)} src="/images/CEO.png" className={loading ? "d-none" :  "w-75 align-middle rounded-circle shadow"} alt="avatar" />
+          <img onLoad={() => setLoading(false)} src="/images/CEO.png" className={loading ? "d-none" :  "w-100 align-middle rounded-circle shadow"} alt="avatar" />
           <div className="d-none d-md-block pt-2"><ProfileData /></div>
         </div>
 
