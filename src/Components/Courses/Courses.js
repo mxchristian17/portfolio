62aae0e1c4ff4295d4a5a0b65233702cea3efd45
mxchristian17 <90223948+mxchristian17@users.noreply.github.com>
2022-03-27 21:39:36 -0300

    Courses and certifications article added. WeatherWidget relocated. Sections HTML5 improved

--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -6,7 +6,7 @@ export const Courses = () => {
   return (
     <div className="col col-lg-10 text-light text-start py-4 px-4">
         <h3 className="pb-4">Courses and certifications</h3>
-        <p><FontAwesomeIcon icon={ faGraduationCap } /> CoderHouse - React Js (Top10% of 120 students)<a className="mx-2 btn btn-sm btn-outline-light" target="_blank" href="https://www.coderhouse.com/certificados/61e7654e6dd5d6007ce70afd?lang=es">Show certificate</a></p>
+        <p><FontAwesomeIcon icon={ faGraduationCap } /> CoderHouse - React Js (Top10% of 120 students)<a className="mx-2 btn btn-sm btn-outline-light" target="_blank" rel="noreferrer" href="https://www.coderhouse.com/certificados/61e7654e6dd5d6007ce70afd?lang=es">Show certificate</a></p>
         <p><FontAwesomeIcon icon={ faGraduationCap } /> Massachusetts Institute of Technology - Introduction to Computer Science and Programming Using Python - MITx 6.00.1x</p>
     </div>
   )
