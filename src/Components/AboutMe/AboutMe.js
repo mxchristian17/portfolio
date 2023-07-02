06fce6548b2ead8cd7878ccd308fb5a363ced5fb
Orengia Christian <orengiachristian@gmail.com>
2023-07-02 19:59:10 -0300

    about me updated

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -4,11 +4,11 @@ const AboutMe = () => {
   return (
     <div className="col-lg-6 text-start px-4 py-3">
         <h3 className="pb-1">About me</h3>
-        <p>I'm passionate about programming. In the last 12 years I spent thousand of hours doing projects and learning several programming languages like PHP, SQL, JavaScript, HTML5 and Python. I'm very self taught and always predisposed to learn new things.<br />
-        According to the LinkedIn PHP skill test, I'm in the top 5% of 505.5K people who did it.<br />
-        Since 2016 I have been developing a complex business management system focused on connecting and optimizing all areas of medium-sized manufacturing production companies.</p>
-        <p>On the other hand, I'm a Mechanical Engineer with experience designing and supervising production of can seamers and filling machines up to 300 CPM, designing and supervising structure construction for the oil and energy industry and software development to manage and coordinate productive processes.<br />
-        Currently I work in a team made of mechanical engineers and mechanical technicians with people in charge developing and producing hi-tech machines mainly for the food industry.</p>
+        <p>I am passionate about programming and have dedicated the past 12 years to mastering various programming languages such as PHP, SQL, JavaScript, HTML5, and Python. I am a self-taught individual with a strong drive for continuous learning and exploration.</p>
+        <p>In terms of proficiency, I have successfully completed the LinkedIn PHP skill test, ranking in the top 5% out of 847.7K participants. Similarly, I achieved a top-five percentile ranking in the JavaScript skill test, placing among the top 5% out of 2.5M participants.</p>
+        <p>Since 2016, I have been actively involved in developing a sophisticated business management system specifically designed to optimize operations in medium-sized manufacturing production companies. This comprehensive solution focuses on enhancing communication and efficiency across all areas of the organization.</p>
+        <p>Furthermore, I bring a unique skill set as a Mechanical Engineer with experience in designing and supervising the production of can seamers and filling machines, capable of handling up to 300 CPM. Additionally, I have been involved in structural construction projects for the oil and energy industry and have developed software solutions to manage and coordinate production processes.</p>
+        <p>Currently, I am an integral part of a cross-functional team consisting of skilled mechanical engineers and technicians. Together, we collaborate on developing and delivering cutting-edge machines, with a primary focus on serving the food industry.</p>
     </div>
   )
 }
